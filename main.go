a9396400b600fc6284221291e1939af17379162a
bigyanse <contact.bigyanse@gmail.com>
2024-09-28 12:25:27 +0545

    `refactor`: ♻️ refactored controllers to own folders

--- main.go
+++ main.go
@@ -1,168 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
-	"sync"
+
+	"github.com/bigyanse/go-crud/controllers"
 )
-
-type Product struct {
-	Id    int         `json:"id"`
-	Title string      `json:"title"`
-	Price json.Number `json:"price"`
-}
-
-var products = make(map[int]Product)
-var mutex sync.RWMutex
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /products", GetProducts)
-	mux.HandleFunc("GET /products/{id}", GetProduct)
-	mux.HandleFunc("POST /products", CreateProduct)
-	mux.HandleFunc("PUT /products/{id}", UpdateProduct)
-	mux.HandleFunc("DELETE /products/{id}", DeleteProduct)
+	mux.HandleFunc("GET /products", controllers.GetProducts)
+	mux.HandleFunc("GET /products/{id}", controllers.GetProduct)
+	mux.HandleFunc("POST /products", controllers.CreateProduct)
+	mux.HandleFunc("PUT /products/{id}", controllers.UpdateProduct)
+	mux.HandleFunc("DELETE /products/{id}", controllers.DeleteProduct)
 
 	fmt.Println("Server listening to :8080")
 	http.ListenAndServe(":8080", mux)
-}
-
-func GetProducts(w http.ResponseWriter, r *http.Request) {
-	mutex.RLock()
-	productsArray := []Product{}
-	for _, v := range products {
-		productsArray = append(productsArray, v)
-	}
-	mutex.RUnlock()
-
-	if len(products) < 1 {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("no data"))
-		return
-	}
-
-	data, err := json.Marshal(productsArray)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
-}
-
-func GetProduct(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	mutex.RLock()
-	product, ok := products[id]
-	mutex.RUnlock()
-
-	if !ok {
-		http.Error(w, "product not found", http.StatusNotFound)
-		return
-	}
-
-	data, err := json.Marshal(product)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
-}
-
-func CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product Product
-
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		http.Error(w, "please provide all fields as designated (title as string and price as number)", http.StatusBadRequest)
-		return
-	}
-
-	if product.Title == "" || product.Price == "" {
-		http.Error(w, "all fields required (title, price)", http.StatusBadRequest)
-		return
-	}
-
-	mutex.Lock()
-	product.Id = len(products) + 1
-	products[product.Id] = product
-	mutex.Unlock()
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("done"))
-}
-
-func UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	mutex.RLock()
-	_, ok := products[id]
-	mutex.RUnlock()
-
-	if !ok {
-		http.Error(w, "product not found", http.StatusBadRequest)
-		return
-	}
-
-	var product Product
-
-	err = json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		http.Error(w, "please provide all fields as designated (title as string and price as number)", http.StatusBadRequest)
-		return
-	}
-
-	if product.Title == "" || product.Price == "" {
-		http.Error(w, "all fields required (title, price)", http.StatusBadRequest)
-		return
-	}
-
-	mutex.Lock()
-	product.Id = id
-	products[id] = product
-	mutex.Unlock()
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("done"))
-}
-
-func DeleteProduct(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	mutex.RLock()
-	_, ok := products[id]
-	mutex.RUnlock()
-
-	if !ok {
-		http.Error(w, "product not found", http.StatusBadRequest)
-		return
-	}
-
-	mutex.Lock()
-	delete(products, id)
-	mutex.Unlock()
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("done"))
 }
